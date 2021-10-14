88a909644819552cfc07eb96a677744d1db4ebee
Chandra Lama <iamme24cl@gmail.com>
2021-10-14 08:32:20 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/login_status')
+      .then(res => console.log(res.data));
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post('http://localhost:3000/users', {
+      user: {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password
+      }
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+    this.setState({
+      name: "",
+      email: "",
+      password: ""
+    })
+  }
+
+
+
+  render () {
+    return (
+      <div className="container mt-5">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange} className="form-control"/>
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="text" placeholder="example@mail.com" value={this.state.email} name="email" onChange={this.handleChange} className="form-control"/>
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" placeholder="password" value={this.state.password} name="password" onChange={this.handleChange} className="form-control"/>
+          </div>
+
+          <div className="form-group">
+            <input type="submit" className="btn btn-dark"/>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
